7a96efc2257291e13900d14bda23a83a4cfdd588
Kamil Konopka <kamil.konopka@thomsonreuters.com>
2018-12-01 19:54:51 +0100

    added lifecycle hooks walkthrough

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import classes from './Cockpit.css';
+import Aux from '../../hoc/Aux';
 
 const cockpit = (props) => {
     const assignedClasses = [];
-    let btnClass = '';
+    let btnClass = classes.Button;
 
     if(props.showPersons) {
-        btnClass = classes.red;
+        btnClass = [classes.Button, classes.red].join(' ');
     }
 
     if (props.persons.length <= 2) {
@@ -16,14 +17,14 @@ const cockpit = (props) => {
       assignedClasses.push(classes.bold);
     }
     return(
-        <div className={classes.Cockpit}>
+        <Aux >
         <h1>{props.appTitle}</h1>
         <p className={assignedClasses.join(' ')}>This is really working!</p>
         <button 
             className={btnClass}
             onClick={props.clicked}
             >Toggle Persons</button>
-        </div>
+        </Aux>
     );
 }
 
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import styling from './Person.css'
 
 class Person extends Component {
+
+    constructor(props) {
+        super(props);
+        console.log('[Person.js] Inside Constructor', props);
+      }
+    
+      componentWillMount() {
+        console.log('Person.js] Inside componentWillMount()');
+      }
+    
+      componentDidMount() {
+        console.log('Person.js] Inside componentDidMount()');
+      }
+
     render() {
         return(
             <div className={styling.Person} >
