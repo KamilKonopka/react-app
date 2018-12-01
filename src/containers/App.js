f836d4bf74208b90abdd235156374fca1f3c3e70
Kamil Konopka <kamil.konopka@thomsonreuters.com>
2018-12-01 19:56:50 +0100

    added Auxillary file as an empty container to keep multiple elements without any wrapping html tag

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,44 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import styling from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 
-class App extends Component {
-  state = {
-    persons: [
-      {id: '1', name: 'Max', age: 28},
-      {id: '2', name: 'Manu', age: 29},
-      {id: '3', name: 'Stephanie', age: 26}
-    ]
-  };
+class App extends PureComponent {
+
+  constructor(props) {
+    super(props);
+    console.log('[App.js] Inside Constructor', props);
+    this.state = {
+      persons: [
+        {id: '1', name: 'Max', age: 28},
+        {id: '2', name: 'Manu', age: 29},
+        {id: '3', name: 'Stephanie', age: 26}
+      ]
+    };
+  }
+
+  componentWillMount() {
+    console.log('[App.js] Inside componentWillMount()');
+  }
+
+  componentDidMount() {
+    console.log('[App.js] Inside componentDidMount()');
+  }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('[Update App.js] Inside shouldComponentUpdate()', nextProps, nextState);
+  //   return nextState.persons !== this.state.persons ||
+  //   nextState.showPersons !== this.state.persons;
+  // }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('[Update App.js] Inside componentWillUpdate()', nextProps, nextState);
+}
+
+  componentDidUpdate() {
+    console.log('[Update App.js] Inside componentDidUpdate()'); 
+}
 
   switchNameHandler = (newName) => {
     // console.log('clicked');
@@ -59,6 +86,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('[App.js] Inside render()');
     let persons = null;
 
     if (this.state.showPersons) {
@@ -70,6 +98,7 @@ class App extends Component {
 
     return (
       <div className={styling.App}>
+      <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
       <Cockpit 
         appTitle={this.props.title}
         showPersons={this.state.showPersons}
