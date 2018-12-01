ebfdb48401d2799b462f2e12e87a7ed0abcb5e9d
Kamil Konopka <kamil.konopka@thomsonreuters.com>
2018-12-01 14:55:20 +0100

    cleaning the app before turning Person and Persons from functional components to class components

--- src/containers/App.js
+++ src/containers/App.js
@@ -71,6 +71,7 @@ class App extends Component {
     return (
       <div className={styling.App}>
       <Cockpit 
+        appTitle={this.props.title}
         showPersons={this.state.showPersons}
         persons={this.state.persons}
         clicked={this.togglePersonsHandler}
