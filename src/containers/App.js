cdc133e1506e143df74c30bedb2278585f2055a0
Kamil Konopka <kamil.konopka@thomsonreuters.com>
2018-12-01 13:43:05 +0100

    reorganized file structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import styling from './App.css';
+import Person from '../components/Persons/Person/Person';
+import ErrorBoundry from '../components/ErrorBoundry/ErrorBoundry';
 
 class App extends Component {
   state = {
@@ -57,49 +58,40 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid green',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person 
+            return <ErrorBoundry key={person.name} ><Person 
             name={person.name} 
             age={person.age} 
-            key={person.name} 
             click={() => this.deletePersonHandler(index)}
             changed={(event) => this.nameChangedHandler(event, person.id)}
             />
+            </ErrorBoundry>
           })}
         </div>
       );
-
-      style.backgroundColor = 'red';
+      btnClass = styling.red;
     }
 
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(styling.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(styling.bold);
     }
     return (
-      <div className="App">
+      <div className={styling.App}>
         <h1>Hi, I'm React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
         <button 
-        style={style}
+        className={btnClass}
         onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './Person/Person.css';
-import App from './App';
+import './components/Persons/Person/Person.css';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
